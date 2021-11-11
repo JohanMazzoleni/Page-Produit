1173ceb68af934ccc1bf20654777f5f1012f2298
JohanMazzoleni <85082764+JohanMazzoleni@users.noreply.github.com>
2021-11-11 23:12:27 +0100

    feat(front-end): add header component

---
+++ components/Header.vue
@@ -0,0 +1,115 @@
+<script>
+export default {
+	data() {
+		return {
+			productsInCart: [
+				{
+					name: "Fall Limited Edition Sneakers",
+					image: require("@/assets/images/image-product-1-thumbnail.jpg"),
+					price: 125,
+					quantity: 3,
+				},
+				{
+					name: "Fall Limited Edition Sneakers",
+					image: require("@/assets/images/image-product-1-thumbnail.jpg"),
+					price: 125,
+					quantity: 3,
+				},
+				{
+					name: "Fall Limited Edition Sneakers",
+					image: require("@/assets/images/image-product-1-thumbnail.jpg"),
+					price: 125,
+					quantity: 3,
+				},
+			],
+		};
+	},
+};
+</script>
+
+<template>
+	<header class="header">
+		<nav>
+			<ul>
+				<li class="menu-icon" title="Menu">
+					<img
+						:src="require('@/assets/images/icon-menu.svg')"
+						alt="Menu"
+						class="menu-icon"
+					/>
+				</li>
+				<li class="name">
+					<a href="javascript:void(1);"
+						><img
+							:src="require('@/assets/images/logo.svg')"
+							alt="Logo"
+					/></a>
+				</li>
+				<li class="item">
+					<a href="javascript:void(1);">Collections</a>
+				</li>
+				<li class="item"><a href="javascript:void(1);">Men</a></li>
+				<li class="item"><a href="javascript:void(1);">Women</a></li>
+				<li class="item"><a href="javascript:void(1);">About</a></li>
+				<li class="item"><a href="javascript:void(1);">Contact</a></li>
+			</ul>
+		</nav>
+		<div class="right">
+			<div class="basket" title="Panier d'achat" tabindex="0">
+				<img
+					class="basket-img"
+					:src="require('@/assets/images/icon-cart.svg')"
+					alt="Panier d'achat"
+				/>
+				<div class="basket-tab">
+					<div class="title">Cart</div>
+					<div class="empty-cart" v-if="productsInCart.length === 0">
+						Your card is empty.
+					</div>
+					<div class="body-cart" v-if="productsInCart.length >= 1">
+						<div
+							class="card"
+							v-for="(value, index) in productsInCart"
+							:key="index"
+						>
+							<div class="image">
+								<img :src="value.image" :alt="value.name" />
+							</div>
+							<div class="details">
+								<p>{{ value.name }}</p>
+								<p>
+									${{ value.price }} x {{ value.quantity }}
+									<span class="price"
+										>${{
+											value.price * value.quantity
+										}}</span
+									>
+								</p>
+							</div>
+							<div class="trash">
+								<img
+									:src="
+										require('@/assets/images/icon-delete.svg')
+									"
+									alt="Supprimer ce produit"
+									title="Supprimer ce produit"
+								/>
+							</div>
+						</div>
+						<div class="btn">
+							<a class="btn-style" href="javascript:void(1)"
+								>Checkout</a
+							>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="profil" title="Photo de profil" tabindex="0">
+				<img
+					:src="require('@/assets/images/image-avatar.png')"
+					alt="Photo de profil"
+				/>
+			</div>
+		</div>
+	</header>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
 	<div>
-		Hello
+		<Header></Header>
 	</div>
 </template>
