ad1fc1d29828fde392292e2194c71bbef04cb473
JohanMazzoleni <85082764+JohanMazzoleni@users.noreply.github.com>
2021-11-12 09:50:02 +0100

    feat(front-end): add detail product

--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,13 @@ export default {
 		return {
 			selectedImage: 0,
 			openImage: 0,
+			quantity: 1,
+			maxQuantity: 50,
 			assets: {
 				icon: {
 					previous: require("@/assets/images/icon-previous.svg"),
 					next: require("@/assets/images/icon-next.svg"),
+					icon_cart: require("@/assets/images/icon-cart-white.svg"),
 				},
 				productImage: [
 					require("@/assets/images/image-product-1.jpg"),
@@ -37,6 +40,21 @@ export default {
 				let newIndex = ctx.selectedImage + 1;
 				if (ctx.assets.productImage[newIndex]) {
 					ctx.selectedImage = newIndex;
+				}
+			}
+		},
+		switchQuantity(state) {
+			var ctx = this;
+
+			if (state === 0) {
+				let newIndex = ctx.quantity - 1;
+				if (newIndex >= 0 && ctx.quantity <= ctx.maxQuantity) {
+					ctx.quantity = newIndex;
+				}
+			} else {
+				if (ctx.quantity < ctx.maxQuantity) {
+					let newIndex = ctx.quantity + 1;
+					ctx.quantity = newIndex;
 				}
 			}
 		},
@@ -106,7 +124,50 @@ export default {
 						/>
 					</div>
 				</div>
-				<div class="details"></div>
+				<div class="details">
+					<p class="tag">SNEAKER COMPANY</p>
+					<h1>Fall Limited Edition <br />Sneakers</h1>
+					<p class="description">
+						These low-profile sneakers are your perfect casual
+						wear<br />
+						companion. Featuring a durable rubber outer sole,
+						they’ll<br />
+						withstand everything the weather can offer.
+					</p>
+					<div class="price-menu">
+						<div class="price">$125.00</div>
+						<div class="percent">50%</div>
+					</div>
+					<div class="reduction">$250.00</div>
+					<div class="buy-menu">
+						<div class="quantity-selector">
+							<div
+								class="less"
+								title="Réduire"
+								v-on:click="switchQuantity(0)"
+							>
+								-
+							</div>
+							<div class="quantity">
+								{{ quantity }}
+							</div>
+							<div
+								class="more"
+								title="Augmenter"
+								v-on:click="switchQuantity(1)"
+							>
+								+
+							</div>
+						</div>
+						<a class="buy-button" href="javascript:void(1)">
+							<img
+								:src="assets.icon.icon_cart"
+								alt="Icone d'achat"
+							/>
+							Add to cart
+						</a>
+					</div>
+				</div>
 			</section>
 		</main>
 	</div>
